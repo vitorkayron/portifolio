f3c208f069b0807a3e952d4dff664d33c55425f2
Vitor Kayron <vitork2016@gmail.com>
2023-02-20 10:47:25 -0300

    Add superuser

--- projetos/apps.py
+++ projetos/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class ProjetosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projetos'
+
+    def ready(self):
+        import os
+        User = get_user_model()
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = User.objects.filter(email=email)
+        if not usuarios:
+            User.objects.create_superuser(username="admin", email=email, senha=senha, is_active=True, is_staff=True)
